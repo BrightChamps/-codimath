4e929565d5d7d09c694b80a3a78d49e4ec53ad61
vatsal3333 <vatsalseth33@gmail.com>
2020-11-30 11:01:21 +0530

    Navigation Bar Added in Explore Section

--- Explore Section/Rectangle/block/diagnol.js
+++ Explore Section/Rectangle/block/diagnol.js
@@ -1,7 +1,7 @@
 Blockly.Blocks['diagnol'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Select Diagnol to Cut Through");
+        .appendField("Select Diagonal to Cut Through");
     this.appendDummyInput()
         .appendField(new Blockly.FieldDropdown([["Top-Left to Bottom-Right","1"], ["Bottom-Left to Top-Right","2"]]), "NAME");
     this.setPreviousStatement(true, null);
--- Explore Section/Rectangle/block/start.js
+++ Explore Section/Rectangle/block/start.js
@@ -1,7 +1,7 @@
 Blockly.Blocks['start'] = {
     init: function() {
       this.appendDummyInput()
-          .appendField("Start Creating Rectanle of");
+          .appendField("Start Creating Rectangle of");
       this.setNextStatement(true, null);
       this.setColour(230);
    this.setTooltip("");
